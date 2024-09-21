d9e1865625d591450b0406ddbf952b44007127ff
Azra-Javed <azraj063@gmail.com>
2024-09-21 13:55:57 +0500

    Refactor the game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,10 +10,8 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const { data, isLoading } = useGames(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
-
-  if (error) return <Text>{error}</Text>;
 
   return (
     <SimpleGrid
